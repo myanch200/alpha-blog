cd60161958b5606c2c95739dcb609637b1380a4e
Martin Yanchev <myanch200@gmail.com>
2021-12-21 16:55:25 +0000

    Adding show to view and controller for article

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
-  get "/about", to: "pages#about"
+  get "about", to: "pages#about"
+  get "articles/:id", to: "articles#show"
+
 end
