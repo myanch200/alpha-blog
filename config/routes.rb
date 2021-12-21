fe1bd5691539256328460831d227cd85dc029529
Martin Yanchev <myanch200@gmail.com>
2021-12-21 17:12:19 +0000

    add articles list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#index"
   get "about", to: "pages#about"
-  get "articles/:id", to: "articles#show"
+
+  resources :articles 
 
 end
